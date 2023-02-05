e0deaecd688a36f053d83eab0fd5e026257746ca
unknown <btylmz7@gmail.com>
2023-02-05 15:19:39 +0300

    Add VariableProvider project

---
+++ VariableProvider/VariableProvider/VariableProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Flee;
+
+namespace VariableProvider
+{
+    public sealed class VariableProvider
+    {
+        #region Fields
+        private static VariableProvider Instance = null;
+        private static object InstanceLock = new object();
+        #endregion
+
+        #region Constructor
+        private VariableProvider() { }
+        #endregion
+
+        #region Instance 
+        public static void CreateInstance()
+        {
+            if (Instance is null)
+            {
+                lock (InstanceLock)
+                {
+                    if (Instance is null)
+                    {
+                        Instance = new VariableProvider();
+                    }
+                }
+
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ VariableProvider/VariableProviderTest/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommunityToolkit.Mvvm;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VariableProviderTest.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
